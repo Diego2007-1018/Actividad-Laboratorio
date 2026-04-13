ee8f880b38105eed46c5c68c4f050b68b44fffb3
Diego2007-1018 <diegoimbpc28@gmail.com>
2026-04-13 09:39:53 -0600

    pequeño cesar tarea :D
    holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ Bloque2/PequeCesar.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="PequeCesar.css">
+    <title>Curso de PHP - 5to Laboratorio</title>
+</head>
+<body>
+    <header>
+        <h1>Little Caesars</h1>
+    </header>
+</body>
+<p></p>
+
+<div>
+    <!-- Formulario -->
+     <form action="welcome.php" method="post">
+        <label for="nombre">Nombre:</label>
+        <input type="text" id="nombre" name="nombre" placeholder="Ingrese su nombre" required><br><br>
+        
+        <label for="apellido">Apellido:</label>
+        <input type="text" id="apellido" name="apellido" placeholder="Ingrese su apellido" required><br><br>
+
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email" placeholder="Ingrese su email" required><br><br>
+
+        <label for="Contraseña">Contraseña</label>
+        <input type="password" id="password" name="password" placeholder="Ingrese su contraseña" required><br><br>
+
+        <input type ="Submit">
+
+</form>
+
+</div>
+</html>
+
+<!-- para que funcione el formulario -->
+<?php
+ if (isset ($_Post ["nombre"])){
+    echo "Hola" . $_Post ["nombre"];
+ }
+?>
+
+
+<?php
+ if (isset ($_Post ["apellido"])){
+    echo "" . $_Post ["apellido"];
+ }
+?>
+
+
+<?php
+ if (isset ($_Post ["email"])){
+    echo "Su imail fue aceptada" . $_Post ["email"];
+ }
+?>
+
+
+<?php
+ if (isset ($_Post ["password"])){
+    echo "Su contraseña fue aceptada" . $_Post ["password"];
+ }
+?>
+
