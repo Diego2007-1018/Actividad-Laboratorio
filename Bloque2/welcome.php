1d02f1b7f74794431ee57325993d155575606224
Diego2007-1018 <diegoimbpc28@gmail.com>
2026-04-24 13:30:18 -0600

    Peque cesar mejorado 2.0 Vamos por uno el Lunes :D
    añjsghdoijabsoidfgbsodfv
    ws
    f
    asf
    sw
    fd
    s
    fsdfs
    df
    s
    d
    f
    s
    df
    s
    df
    s
    e

--- Bloque2/welcome.php
+++ Bloque2/welcome.php
@@ -1,15 +1,22 @@
-
-<!--- Para recibir la info --->
-
 <?php
 if (isset($_POST['monto'])){
-    echo "Si hay monto a cobrar";
-}
+    echo "Si hay monto a cobrar<br>";
+    
+    $monto = $_POST['monto'];
 
-if ($_POST['monto'] >= 30){
-    echo "El monto total a pagar con descuento es: " . $_POST['monto'] * 0.10;
-} else {
-    echo "No aplica descuento del 10%";
+    // Validación según los rangos de la imagen
+    if ($monto > 300 && $monto < 500) {
+        // Descuento del 42%
+        echo "El monto total a pagar con descuento es: " . ($monto - ($monto * 0.42));
+    } elseif ($monto > 80 && $monto < 200) {
+        // Descuento del 25%
+        echo "El monto total a pagar con descuento es: " . ($monto - ($monto * 0.25));
+    } elseif ($monto > 30 && $monto <= 79) {
+        // Descuento del 10%
+        echo "El monto total a pagar con descuento es: " . ($monto - ($monto * 0.10));
+    } else {
+        echo "No aplica descuento";
+    }
 }
 ?>
 <!DOCTYPE html>
