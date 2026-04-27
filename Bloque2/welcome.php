f333d10e17fac61a175f6a21dfd04c1edc0222f6
Diego2007-1018 <diegoimbpc28@gmail.com>
2026-04-27 11:27:34 -0600

    holaaaaaaaa profe aquí ta mi peque cesar más mejor
    hay un easter egg en este parrafo 
    uihasgvd
    asd
    ja
    osda
    rdas
    gda
    es
    eda
    sd
    ga
    ad
    yasda
    d
    a
    da
    d
    a
    da
    d
    ada
    da
    d
    a
    dad
    a
    d
    asda
    sw
    peque 
    qe
    wq
    eq
    we
    q
    we
    q
    we
    qe
    q
    we
    q
    ew
    eq
    cesar
    as
    da
    s
    da
    d
    sa
    daw
    da
    s
    fafvascas
    d
    asdac
    f
    as
    fa
    sf

--- Bloque2/welcome.php
+++ Bloque2/welcome.php
@@ -3,21 +3,25 @@ if (isset($_POST['monto'])){
     echo "Si hay monto a cobrar<br>";
     
     $monto = $_POST['monto'];
-
+    $produc = "";
     // Validación según los rangos de la imagen
     if ($monto > 300 && $monto < 500) {
         // Descuento del 42%
-        echo "El monto total a pagar con descuento es: " . ($monto - ($monto * 0.42));
+        $produc = "Como la compra fue mayor a 300 se le aplica el descuento del 42%";
+        echo "El monto total a pagar con descuento es: " . ($monto - ($monto * 0.42)) . ($produc);
     } elseif ($monto > 80 && $monto < 200) {
         // Descuento del 25%
-        echo "El monto total a pagar con descuento es: " . ($monto - ($monto * 0.25));
+        $produc = "Como la compra fue mayor a 80 se le aplica el descuento del 25%";
+        echo "El monto total a pagar con descuento es: " . ($monto - ($monto * 0.25)) . ($produc);
     } elseif ($monto > 30 && $monto <= 79) {
         // Descuento del 10%
-        echo "El monto total a pagar con descuento es: " . ($monto - ($monto * 0.10));
+        $produc = "Como la compra fue mayor a 30 se le aplica el descuenti del 10%";
+        echo "El monto total a pagar con descuento es: " . ($monto - ($monto * 0.10)) . ($produc);
     } else {
         echo "No aplica descuento";
     }
-}
+} 
+
 ?>
 <!DOCTYPE html>
 <html lang="es">
